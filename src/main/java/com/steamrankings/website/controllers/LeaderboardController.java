b5837c155e176eb0c24bf514cd076443a7e8f9c8
sryan28 <sean.ryan3@mail.mcgill.ca>
2015-02-27 21:52:48 -0500

    commit

--- src/main/java/com/steamrankings/website/controllers/LeaderboardController.java
+++ src/main/java/com/steamrankings/website/controllers/LeaderboardController.java
@@ -16,15 +16,15 @@ public class LeaderboardController {
 
     
     @ModelAttribute("rankentries")
-    public List<RankEntryByAchievements> populateTable() {
-        RankEntryByAchievements r1 = new RankEntryByAchievements(1, 1234, "swag",
+   // public List<RankEntryByAchievements> populateTable() {
+       /* RankEntryByAchievements r1 = new RankEntryByAchievements(1, 1234, "swag",
         500, "50", "Canada"); RankEntryByAchievements r2 = new
         RankEntryByAchievements(2, 4321, "yolo", 501, "51", "USA");
         List<RankEntryByAchievements> curList = new
         ArrayList<RankEntryByAchievements>(); curList.add(r1); curList.add(r2);
 
-        return curList;
-    }
+        return curList;*/
+    //}
     
 
     @RequestMapping("/leaderboard")
@@ -43,7 +43,7 @@ public class LeaderboardController {
     private void byAchievements(Model model) {
         List<RankEntryByAchievements> rankEntries = Leaderboards.getRanksByAchievementTotal(1, 10);
 
-        if (rankEntries != null) {
+       /* if (rankEntries != null) {
             for (int i = 0; i < rankEntries.size(); i++) {
                 if (rankEntries.get(i).getCountryCode() != null && !rankEntries.get(i).getCountryCode().equals("")) {
                     String countryFlag = "/assets/images/country_flags/" + rankEntries.get(i).getCountryCode().toLowerCase() + ".png";
@@ -58,6 +58,6 @@ public class LeaderboardController {
             model.addAttribute("rankentries", rankEntries);
         } else {
         	populateTable();
-        }    	
+        }    	*/
     }
 }
